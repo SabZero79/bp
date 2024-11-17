6e392bbae2a9689a305d5b0bf17ee93a7ed25eb7
SabZero79 <160794548+SabZero79@users.noreply.github.com>
2024-11-17 19:53:35 +0200

    Added Selenium Tests

--- TestProject/Features/BloodPressureCategory.feature.cs
+++ TestProject/Features/BloodPressureCategory.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace TestProject.Features
+namespace Unit_BDD_Tests.Features
 {
     using TechTalk.SpecFlow;
     using System;
